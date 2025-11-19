a2508bdf0231f7eb879f1584c50f29c08503b53e
Jaswanth1406 <jaswanthprasanna68@gmail.com>
2025-11-19 16:47:15 +0530

    Backend of the SaaS Login System

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+export default function auth(req , res , next){
+
+    const header = req.headers.authorization;
+    if(!header) return res.status(401).json({message: "Authorization header missing"});
+
+    const token = header.split(" ")[1];
+    if(!token) return res.status(401).json({message: "Token missing"});
+
+    try{
+        const decoded = jwt.verify(token , process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }catch{
+        return res.status(401).json({message: "Invalid token"});
+    }
+}
